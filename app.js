4d9a1a2fc5e53add1b8dd2cb2eeed172853bc600
Maksimilianoo <shtefanyukmaksym@gmail.com>
2023-07-07 15:03:07 +0300

    100%

--- app.js
+++ app.js
@@ -3,52 +3,52 @@ const todoForm = document.getElementById("todoForm");
 const todoList = document.getElementById("todoList");
 
 const renderList = () => {
-  todoList.innerHTML = "";
-  let content = "";
+    todoList.innerHTML = "";
+    let content = "";
 
-  todosList.map((todo) => {
-    const ul = `
+    todosList.map((todo) => {
+        const ul = `
   <li>
   <input type="checkbox" id="${todo.id}" onchange="updateStatus(${todo.id})" ${
-      todo.status ? "checked" : ""
-    }>
+            todo.status ? "checked" : ""
+        }>
   <span>${todo.text}</span>
   <button onclick="deleteTodo(${todo.id})" ${
-      !todo.status ? "disabled" : ""
-    }>Delete</button>
+            !todo.status ? "disabled" : ""
+        }>Delete</button>
   </li>
   `;
-    content += ul;
-  });
+        content += ul;
+    });
 
-  todoList.innerHTML = content;
+    todoList.innerHTML = content;
 };
 
 const updateStatus = (id) => {
-  const todo = todosList.find((todo) => todo.id === id);
-  if (todo) {
-    todo.status = !todo.status;
-    renderList();
-  }
+    const todo = todosList.find((todo) => todo.id === id);
+    if (todo) {
+        todo.status = !todo.status;
+        renderList();
+    }
 };
 
 const deleteTodo = (id) => {
-  todosList = todosList.filter((todo) => todo.id !== id);
-  renderList();
+    todosList = todosList.filter((todo) => todo.id !== id);
+    renderList();
 };
 
 todoForm.addEventListener("submit", (e) => {
-  const field = document.getElementById("todoInput");
-  e.preventDefault();
+    const field = document.getElementById("todoInput");
+    e.preventDefault();
 
-  const newTodo = {
-    id: Date.now(),
-    date: new Date(),
-    text: field.value,
-    status: false,
-  };
+    const newTodo = {
+        id: Date.now(),
+        date: new Date(),
+        text: field.value,
+        status: false,
+    };
 
-  field.value = "";
-  todosList.push(newTodo);
-  renderList();
+    field.value = "";
+    todosList.push(newTodo);
+    renderList();
 });
